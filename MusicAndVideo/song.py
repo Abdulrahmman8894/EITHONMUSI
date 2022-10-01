d705e8c280a70ef0ea1a19f093fb8b339afe8a8b
MR_X_ <95410840+Abdulrahmman8894@users.noreply.github.com>
2022-10-01 03:29:30 +0200

    Update song.py

--- MusicAndVideo/song.py
+++ MusicAndVideo/song.py
@@ -23,7 +23,7 @@ async def song(client, message: Message):
     if not urlissed:
         await client.send_message(
             message.chat.id,
-            "-› يرجى اعطاء اسم الاغنية او راجع زر الاوامر لمعرفة استخدامي ⚡️.",
+            "-› يرجى اعطاء اسم السورة او راجع زر الاوامر لمعرفة استخدامي ⚡️.",
         )
         return
     pablo = await client.send_message(
@@ -242,7 +242,7 @@ async def vsong(client, message: Message):
     )
     if not urlissed:
         await pablo.edit(
-            "-› يرجى اعطاء اسم الاغنية او راجع زر الاوامر لمعرفة استخدامي ⚡️."
+            "-› يرجى اعطاء اسم السورة او راجع زر الاوامر لمعرفة استخدامي ⚡️."
         )
         return
 
@@ -293,7 +293,7 @@ async def vsong(client, message: Message):
         progress_args=(
             pablo,
             c_time,
-            f"** اسم الاغنيه ⚡️ :** `{urlissed}`",
+            f"** اسم السورة ⚡️ :** `{urlissed}`",
             file_stark,
         ),
     )
